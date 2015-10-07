4f1903e399af8a81feea46c09d82bc82310e8d7f
eshon <eva.shon@consensys.net>
2015-10-06 20:25:49 -0400

    adding tests

--- contracts/Conference.sol
+++ contracts/Conference.sol
@@ -1,105 +1,47 @@
 contract Conference {
 
-	// enum mappings not working so use:
-	
-	uint constant Full = 0;
-	uint constant Developer = 1;
-	uint constant Student = 2;
-	uint constant Mon = 3;
-	uint constant Tue = 4;
-	uint constant Wed = 5;
-	uint constant Thu = 6;
-	uint constant Fri = 7;
-
-	struct Registrant {
-		address addr;
-		uint amount;
-		uint ticketType;
-	}
-	
-	struct TicketDetails {
-		uint price;
-		uint quota;
-		uint count;
-	}
-
-	address owner;
-	Registrant[] registrants; // for refunds
-	mapping (uint => TicketDetails) tickets; // for refunds
-
+	address public owner;
+	mapping (address => uint) public registrantsPaid;
+	mapping (address => string) public registrantsEmail;
+	uint public numRegistrants;
+	uint public quota;
 
 	function Conference() {
-		owner = msg.sender;
-		
-		// PRICE - Do we need use price feed for £ to ETH? Assume prices below in £
-
-		tickets[Full] = TicketDetails({ price: 750, quota: 200, count: 0 });
-
-		// Day Passes
-		tickets[Mon] = TicketDetails({ price: 250, quota: 100, count: 0 });
-		tickets[Tue] = TicketDetails({ price: 250, quota: 100, count: 0 });
-		tickets[Wed] = TicketDetails({ price: 250, quota: 100, count: 0 });
-		tickets[Thu] = TicketDetails({ price: 250, quota: 100, count: 0 });
-		tickets[Fri] = TicketDetails({ price: 250, quota: 100, count: 0 });
-
-		// Discounted Passes
-		tickets[Developer] = TicketDetails({ price: 200, quota: 50, count: 0 });
-		tickets[Student] = TicketDetails({ price: 100, quota: 50, count: 0 });
+		owner = msg.sender;		
+		quota = 350; // confirm quota amount
+		numRegistrants = 0;
 	}
 
 	// Default buy a ticket
 	function () {
-		buyChoice(Full);
+		if (numRegistrants >= quota) { return; }
+		registrantsPaid[msg.sender] = msg.value;
+		numRegistrants++;
+	}
+	
+	// Buy a ticket with email - should this be done at all?
+	function buyTicketWithEmail(string _email) public returns(uint val) {
+		if (numRegistrants >= quota) { return 0; }
+		registrantsPaid[msg.sender] = msg.value;
+		registrantsEmail[msg.sender] = _email;
+		numRegistrants++;
+		return msg.value;
 	}
 
-	function buyChoice(uint choice) returns(bool success) {
-		if (msg.value < tickets[choice].price) return false;
-		if (tickets[choice].count >= tickets[choice].quota) return false;
-
-		registrants[registrants.length++] = 
-		Registrant({ addr: msg.sender, amount: msg.value, ticketType: choice });
-		tickets[choice].count++;
+	function changeQuota(uint newquota) returns(bool success) {
+		if (msg.sender != owner) { return false; }
+		quota = newquota;
 		return true;
 	}
-
-	function buyDeveloper() { buyChoice(1); }
-	function buyStudent() { buyChoice(2); }
-	function buyMonday() { buyChoice(3); }
-	function buyTuesday() { buyChoice(4); }
-	function buyWednesday() { buyChoice(5); }
-	function buyThursday() { buyChoice(6); }
-	function buyFriday() { buyChoice(7); }
-
-	function changeQuota(uint ticketType, uint newquota) returns(bool success) {
-		if (msg.sender != owner) return false;
-		tickets[ticketType].quota = newquota;
-		return true;
-	}
-
-	// GET LIST OF REGISTRANTS
-	/*
-	function getRegistrants() returns(Registrant[]) {
-		return registrants;
-	}
-	*/
 
 	function refundTicket(address recipient, uint amount) returns(bool success) {
-		if (msg.sender != owner) return false;
-		if (removeRegistrant(recipient)) {
-			if (owner.balance > amount) recipient.send(amount);
-			return true;
-		}
-		return false;
-	}
-
-	function removeRegistrant(address regAddress) returns(bool success) {
-		for (uint i = 0; i < registrants.length; i++) {
-			if (registrants[i].addr == regAddress) {
-				delete registrants[i];
-				for (uint m = i; m < registrants.length - 1; m++) {
-					registrants[m] = registrants[m+1];
-				}
-				registrants.length--;
+		if (msg.sender != owner) { return false; }
+		if (registrantsPaid[recipient] == amount) { // this may need to be more within a close enough range?
+			if (owner.balance > amount) { 
+				recipient.send(amount);
+				registrantsPaid[recipient] = 0;
+				registrantsEmail[msg.sender] = '';
+				numRegistrants--;
 				return true;
 			}
 		}
--- test/conference.js
+++ test/conference.js
@@ -1,7 +1,78 @@
+// NOTE: This function can be removed once this bug is merged and released:
+// https://github.com/ethereum/web3.js/pull/307
+var cleanString = function(str) {
+  var newString = "";
+  for (var i = 0; i < str.length; i++) {
+    var code = str.charCodeAt(i);
+    if (code == 0) break;
+    newString += String.fromCharCode(code);
+  }
+  return newString;
+}
+
 contract('Conference', function(accounts) {
-  it("should assert true", function(done) {
-    var example = Conference.at(Conference.deployed_address);
-    assert.isTrue(true);
-    done();
+	console.log(accounts);
+	var owner_account = accounts[0];
+  var sender_account = accounts[1];
+
+  it("Initial conference settings should match", function(done) {
+    var c = Conference.at(Conference.deployed_address);
+  	
+  	Conference.new({sender: owner_account}).then(
+  		function(conference) {
+  			conference.quota.call()
+  			.then(
+  				function(quota) { 
+  					assert.equal(quota, 350, "Quota doesn't match!"); 
+  			}).then(
+  				function() { 
+  					return conference.numRegistrants.call(); 
+  			}).then(
+  				function(num) { 
+  					assert.equal(num, 0, "Registrants doesn't match!");
+  					return conference.owner.call();
+  			}).then(
+  				function(owner) { 
+  					assert.equal(owner, owner_account, "Owner doesn't match!");
+  					done();
+  			}).catch(done);
+  	}).catch(done);
+  });
+
+
+  it("Should let you buy a ticket", function(done) {
+    var c = Conference.at(Conference.deployed_address);
+
+    var initialBalance = web3.fromWei(web3.eth.getBalance(owner_account).toNumber());	
+    console.log("Initial Balance", initialBalance);
+  	
+  	Conference.new({sender: owner_account}).then(
+  		function(conference) {
+  			conference.buyTicketWithEmail({sender: sender_account, value: web3.toWei(1000)}, "email")
+  			.then(
+  				function() {
+  					var newBalance = web3.fromWei(web3.eth.getBalance(owner_account).toNumber());
+  					console.log("New Balance", newBalance);
+  					assert.isAbove(newBalance, initialBalance, "new balance should be greater than initial");
+  					return conference.numRegistrants.call(); 
+  			}).then(
+  				function(num) { 
+  					assert.isAbove(num, 0, "there should be more registrants");
+  					return conference.registrantsPaid.call(sender_account);
+  			}).then(
+  				function(amount) {
+  					assert.isAbove(amount.toNumber(), 0, "Sender's paid but is not listed as paying");	
+  					return conference.registrantsEmail.call(sender_account);
+  			}).then(
+  				function(email) {
+  					assert.equal(email.toString(), "email", "Sender's email does not match");
+  					return web3.fromWei(web3.eth.getBalance(sender_account));
+  			}).then(
+  				function(bal) {
+  					console.log("bal sender", bal.toNumber());
+  					done();
+  			}).catch(done);
+  	}).catch(done);
   });
 });
+
